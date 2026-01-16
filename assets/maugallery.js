3513c7123d3d98a2f853d238b9e4e19391ac8c2f
Mathias BERGER <mathias.codage@gmail.com>
2026-01-16 18:52:06 +0100

    Résolution du problème de navigation dans la modale galerie

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -121,69 +121,19 @@
     },
     prevImage() {
       let activeImage = null;
+      // On cherche l'image qui est actuellement affichée dans la modale
       $("img.gallery-item").each(function() {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
           activeImage = $(this);
         }
       });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+      
+      // On récupère UNIQUEMENT les images visibles à l'écran (celles du filtre actif)
       let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function() {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
+      $(".gallery-item:visible").each(function() {
+        imagesCollection.push($(this));
+      });
 
-      $(imagesCollection).each(function(i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
-        }
-      });
-      next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
-    },
-    nextImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function() {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function() {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
       let index = 0,
         next = null;
 
@@ -192,7 +142,39 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      
+      // On recule d'un cran (ou on va à la fin si on est au début)
+      next =
+        imagesCollection[index - 1] ||
+        imagesCollection[imagesCollection.length - 1];
+      $(".lightboxImage").attr("src", $(next).attr("src"));
+    },
+
+    nextImage() {
+      let activeImage = null;
+      $("img.gallery-item").each(function() {
+        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
+          activeImage = $(this);
+        }
+      });
+      
+      // On récupère UNIQUEMENT les images visibles à l'écran (celles du filtre actif)
+      let imagesCollection = [];
+      $(".gallery-item:visible").each(function() {
+        imagesCollection.push($(this));
+      });
+
+      let index = 0,
+        next = null;
+
+      $(imagesCollection).each(function(i) {
+        if ($(activeImage).attr("src") === $(this).attr("src")) {
+          index = i;
+        }
+      });
+      
+      // On avance d'un cran (ou on revient au début si on est à la fin)
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
